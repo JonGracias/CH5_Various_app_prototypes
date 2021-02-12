4481725aa873277bc018f468c84fa4a791474c58
Jon Gracias <jonpgracias@gmail.com>
2021-02-12 14:00:21 -0500

    Test drive 1 add Intent intent to onSendMessage()

--- app/src/main/java/com/hfad/mymessanger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessanger/CreateMessageActivity.java
@@ -2,6 +2,7 @@ package com.hfad.mymessanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,8 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view){
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
 
     }
 
