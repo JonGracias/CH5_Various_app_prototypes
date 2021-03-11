0856e306d50f22da0748cc98eb5bdf1b516b4723
Jon Gracias <jonpgracias@gmail.com>
2021-03-11 15:52:06 -0500

    Last Commit

--- app/src/main/java/com/hfad/mymessanger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessanger/CreateMessageActivity.java
@@ -2,10 +2,12 @@ package com.hfad.mymessanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -13,9 +15,24 @@ public class CreateMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Messenger Started";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void onSendMessage(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Sending Message";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
         EditText messageView = findViewById(R.id.message);
         String messageText = messageView.getText().toString();
         //Intent intent = new Intent(this, ReceiveMessageActivity.class);
